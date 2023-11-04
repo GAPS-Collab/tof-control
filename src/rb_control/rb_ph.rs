9f072b7513b5b5e7223079bdb1dad0405b1a99dd
Achim Stoessl <achim.stoessl@gmail.com>
2023-11-03 22:45:51 -1000

    REFACTOR - replace expect with match
    * don't panic!

--- src/rb_control/rb_ph.rs
+++ src/rb_control/rb_ph.rs
@@ -1,3 +1,4 @@
+use log::error;
 use crate::constant::*;
 
 use crate::device::{pca9548a, bme280};
@@ -9,14 +10,38 @@ pub struct RBph {
 
 impl RBph {
     pub fn new() -> Self {
+        let mut pressure = f32::MAX;
+        let mut humidity = f32::MAX;
         let i2c_mux = pca9548a::PCA9548A::new(I2C_BUS, RB_PCA9548A_ADDRESS_1);
 
-        i2c_mux.select(RB_BME280_CHANNEL).expect("cannot accesss to PCA9548A");
+        match i2c_mux.select(RB_BME280_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A");
+          },
+          Ok(_) => ()
+        }
         let bme280 = bme280::BME280::new(I2C_BUS, RB_BME280_ADDRESS);
-        bme280.configure().expect("cannot configure BME280");
-        let (_, pressure, humidity) = bme280.read_data().expect("cannot read BME280");
-
-        i2c_mux.reset().expect("cannot reset PCA9548A");
+        match bme280.configure() {
+          Err(err) => {
+            error!("cannot configure BME280");
+          },
+          Ok(_) => ()
+        }
+        match bme280.read_data() {
+          Err(err) => {
+            error!("cannot read BME280, {err}");
+          },
+          Ok(result) => {
+            pressure = result.1;
+            humidity = result.2;
+          }
+        }
+        match i2c_mux.reset() {
+          Err(err) => {
+            error!("cannot reset PCA9548A, {err}");
+          },
+          Ok(_) => () 
+        }
 
         Self {
             pressure,
--- src/rb_control/rb_vcp.rs
+++ src/rb_control/rb_vcp.rs
@@ -1,3 +1,5 @@
+ use log::error;
+
 use crate::constant::*;
 
 use crate::device::{pca9548a, ina226, max11645};
@@ -34,50 +36,237 @@ impl RBvcp {
         let i2c_mux_1 = pca9548a::PCA9548A::new(I2C_BUS, RB_PCA9548A_ADDRESS_1);
         let i2c_mux_2 = pca9548a::PCA9548A::new(I2C_BUS, RB_PCA9548A_ADDRESS_2);
 
-        i2c_mux_1.select(RB_DRS_DVDD_INA226_CHANNEL).expect("cannot accesss to PCA9548A");
+        match i2c_mux_1.select(RB_DRS_DVDD_INA226_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A, {err}");
+          },
+          Ok(_) => ()
+        }
+
         let drs_dvdd_ina226 = ina226::INA226::new(I2C_BUS, RB_DRS_DVDD_INA226_ADDRESS, RB_DRS_DVDD_INA226_RSHUNT, RB_DRS_DVDD_INA226_MEC);
-        drs_dvdd_ina226.configure().expect("cannot configure INA226 (DRS DVDD)");
-        let (drs_dvdd_voltage, drs_dvdd_current, drs_dvdd_power) = drs_dvdd_ina226.read_data().expect("cannot read INA226 (DRS DVDD)");
-
-        i2c_mux_1.select(RB_P3V3_INA226_CHANNEL).expect("cannot accesss to PCA9548A");
+        match drs_dvdd_ina226.configure() {
+          Err(err) => {
+            error!("cannot configure INA226 (DRS DVDD)");
+          }, 
+          Ok(_) => ()
+        }
+        let mut drs_dvdd_voltage = f32::MAX;
+        let mut drs_dvdd_current = f32::MAX;
+        let mut drs_dvdd_power   = f32::MAX;
+        match drs_dvdd_ina226.read_data() {
+          Err(err) => {
+            error!("cannot read INA226 (DRS DVDD), {err}");
+          },
+          Ok(_result) => {
+            drs_dvdd_voltage = _result.0;
+            drs_dvdd_current = _result.1;
+            drs_dvdd_power   = _result.2;
+          }
+        }
+        match i2c_mux_1.select(RB_P3V3_INA226_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A");
+          },
+          Ok(_) => ()
+        }
         let p3v3_ina226 = ina226::INA226::new(I2C_BUS, RB_P3V3_INA226_ADDRESS, RB_P3V3_INA226_RSHUNT, RB_P3V3_INA226_MEC);
-        p3v3_ina226.configure().expect("cannot configure INA226 (P3V3)");
-        let (p3v3_voltage, p3v3_current, p3v3_power) = p3v3_ina226.read_data().expect("cannot read INA226 (P3V3)");
-
-        i2c_mux_1.select(RB_ZYNQ_INA226_CHANNEL).expect("cannot accesss to PCA9548A");
+        match p3v3_ina226.configure() {
+          Err(err) => {
+            error!("cannot configure INA226 (P3V3)");
+          },
+          Ok(_) => () 
+        }
+        let mut p3v3_voltage = f32::MAX;
+        let mut p3v3_current = f32::MAX;
+        let mut p3v3_power   = f32::MAX;
+        match p3v3_ina226.read_data() {
+          Err(err) => {
+            error!("cannot read INA226 (P3V3), {err}");
+          },
+          Ok(_result) => {
+            p3v3_voltage = _result.0;
+            p3v3_current = _result.1;
+            p3v3_power   = _result.2;
+          }
+        }
+        match i2c_mux_1.select(RB_ZYNQ_INA226_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A, {err}");
+          },
+          Ok(_) => ()
+        }
         let zynq_ina226 = ina226::INA226::new(I2C_BUS, RB_ZYNQ_INA226_ADDRESS, RB_ZYNQ_INA226_RSHUNT, RB_ZYNQ_INA226_MEC);
-        zynq_ina226.configure().expect("cannot configure INA226 (ZYNQ)");
-        let (zynq_voltage, zynq_current, zynq_power) = zynq_ina226.read_data().expect("cannot read INA226 (ZYNQ)");
-
-        i2c_mux_1.select(RB_P3V5_INA226_CHANNEL).expect("cannot accesss to PCA9548A");
+        match zynq_ina226.configure() { 
+          Err(err) => {
+            error!("cannot configure INA226 (ZYNQ)");
+          }
+          Ok(_) => ()
+        }
+        let mut zynq_voltage = f32::MAX;
+        let mut zynq_current = f32::MAX;
+        let mut zynq_power   = f32::MAX;
+        match zynq_ina226.read_data() {
+          Err(err) => {
+            error!("cannot read INA226 (ZYNQ), {err}");
+          },
+          Ok(_result) => {
+            zynq_voltage = _result.0;
+            zynq_current = _result.1;
+            zynq_power   = _result.2;
+          }
+        }
+        match i2c_mux_1.select(RB_P3V5_INA226_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A, {err}");
+          },
+          Ok(_) => () 
+        }
         let p3v5_ina226 = ina226::INA226::new(I2C_BUS, RB_P3V5_INA226_ADDRESS, RB_P3V5_INA226_RSHUNT, RB_P3V5_INA226_MEC);
-        p3v5_ina226.configure().expect("cannot configure INA226 (P3V5)");
-        let (p3v5_voltage, p3v5_current, p3v5_power) = p3v5_ina226.read_data().expect("cannot read INA226 (P3V5)");
-
-        i2c_mux_2.select(RB_ADC_DVDD_INA226_CHANNEL).expect("cannot accesss to PCA9548A");
+        match p3v5_ina226.configure() {
+          Err(err) => {
+            error!("cannot configure INA226 (P3V5), {err}");
+          },
+          Ok(_) => ()
+        }
+        let mut p3v5_voltage = f32::MAX;
+        let mut p3v5_current = f32::MAX;
+        let mut p3v5_power   = f32::MAX;
+        match p3v5_ina226.read_data() {
+          Err(err) => {
+            error!("cannot read INA226 (P3V5), {err}");
+          },
+          Ok(_result) => {
+            p3v5_voltage = _result.0;
+            p3v5_current = _result.1;
+            p3v5_power   = _result.2;
+          }
+        }
+        match i2c_mux_2.select(RB_ADC_DVDD_INA226_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A");
+          },
+          Ok(_) => (),
+        }
         let adc_dvdd_ina226 = ina226::INA226::new(I2C_BUS, RB_ADC_DVDD_INA226_ADDRESS, RB_ADC_DVDD_INA226_RSHUNT, RB_ADC_DVDD_INA226_MEC);
-        adc_dvdd_ina226.configure().expect("cannot configure INA226 (ADC DVDD)");
-        let (adc_dvdd_voltage, adc_dvdd_current, adc_dvdd_power) = adc_dvdd_ina226.read_data().expect("cannot read INA226 (ADC DVDD)");
+        match adc_dvdd_ina226.configure() {
+          Err(err) => {
+            error!("cannot configure INA226 (ADC DVDD)");
+          },
+          Ok(_) => () 
+        }
+        let mut adc_dvdd_voltage = f32::MAX;
+        let mut adc_dvdd_current = f32::MAX;
+        let mut adc_dvdd_power   = f32::MAX;
+        match adc_dvdd_ina226.read_data() {
+          Err(err) => {
+            error!("cannot read INA226 (ADC DVDD)");
+          },
+          Ok(_result) => {
+            adc_dvdd_voltage = _result.0;
+            adc_dvdd_current = _result.1;
+            adc_dvdd_power   = _result.2;
+          }
+        }
 
-        i2c_mux_2.select(RB_ADC_AVDD_INA226_CHANNEL).expect("cannot accesss to PCA9548A");
+        match i2c_mux_2.select(RB_ADC_AVDD_INA226_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A");
+          },
+          Ok(_) => ()
+        }
         let adc_avdd_ina226 = ina226::INA226::new(I2C_BUS, RB_ADC_AVDD_INA226_ADDRESS, RB_ADC_AVDD_INA226_RSHUNT, RB_ADC_AVDD_INA226_MEC);
-        adc_avdd_ina226.configure().expect("cannot configure INA226 (ADC AVDD)");
-        let (adc_avdd_voltage, adc_avdd_current, adc_avdd_power) = adc_avdd_ina226.read_data().expect("cannot read INA226 (ADC AVDD)");
-
-        i2c_mux_2.select(RB_DRS_AVDD_INA226_CHANNEL).expect("cannot accesss to PCA9548A");
+        match adc_avdd_ina226.configure() {
+          Err(err) => {
+            error!("cannot configure INA226 (ADC AVDD)");
+          },
+          Ok(_) => ()
+        }
+        let mut adc_avdd_voltage = f32::MAX;
+        let mut adc_avdd_current = f32::MAX;
+        let mut adc_avdd_power   = f32::MAX;
+        match adc_avdd_ina226.read_data() {
+          Err(err) => {
+            error!("cannot read INA226 (ADC AVDD), {err}");
+          },
+          Ok(_result) => {
+            adc_avdd_voltage = _result.0;
+            adc_avdd_current = _result.1;
+            adc_avdd_power   = _result.2;
+          }
+        }
+        match i2c_mux_2.select(RB_DRS_AVDD_INA226_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A, {err}");
+          },
+          Ok(_) => ()
+        }
         let drs_avdd_ina226 = ina226::INA226::new(I2C_BUS, RB_DRS_AVDD_INA226_ADDRESS, RB_DRS_AVDD_INA226_RSHUNT, RB_DRS_AVDD_INA226_MEC);
-        drs_avdd_ina226.configure().expect("cannot configure INA226 (DRS AVDD)");
-        let (drs_avdd_voltage, drs_avdd_current, drs_avdd_power) = drs_avdd_ina226.read_data().expect("cannot read INA226 (DRS AVDD)");
-
-        i2c_mux_1.select(RB_MAX11645_CHANNEL).expect("cannot accesss to PCA9548A");
+        match drs_avdd_ina226.configure() {
+          Err(err) => {
+            error!("cannot configure INA226 (DRS AVDD), {err}");
+          },
+          Ok(_) => ()
+        }
+        let mut drs_avdd_voltage = f32::MAX;
+        let mut drs_avdd_current = f32::MAX;
+        let mut drs_avdd_power   = f32::MAX;
+        match drs_avdd_ina226.read_data() {
+          Err(err) => {
+            error!("cannot read INA226 (DRS AVDD), {err}");
+          },
+          Ok(_result) => {
+            drs_avdd_voltage = _result.0;
+            drs_avdd_current = _result.1;
+            drs_avdd_power   = _result.2;
+          }
+        }
+        match i2c_mux_1.select(RB_MAX11645_CHANNEL) {
+          Err(err) => {
+            error!("cannot accesss to PCA9548A, {err}");
+          },
+          Ok(_) => ()
+        }
         let max11645 = max11645::MAX11645::new(I2C_BUS, RB_MAX11645_ADDRESS);
-        max11645.setup().expect("cannot configure MAX11645");
-        let n1v5_voltage = max11645.read(RB_N1V5_VOLTAGE_INA200_CHANNEL).expect("cannot read INA200 (N1V5 VOLTAGE)") * -1.0;
-        let n1v5_current = max11645.read(RB_N1V5_CURRENT_INA200_CHANNEL).expect("cannot read INA200 (N1V5 CURRENT)") / 20.0 / 0.039;
+        match max11645.setup() {
+          Err(err) => {
+            error!("cannot configure MAX11645, {err}");
+          },
+          Ok(_) => () 
+        }
+        let mut n1v5_voltage = f32::MAX;
+        let mut n1v5_current = f32::MAX;
+        let mut n1v5_power   = f32::MAX;
+
+        match max11645.read(RB_N1V5_VOLTAGE_INA200_CHANNEL) {
+          Err(err) => {
+            error!("cannot read INA200 (N1V5 VOLTAGE), {err}");
+          },
+          Ok(_result) => {
+            n1v5_voltage = _result* -1.0;
+          }
+        }
+        match max11645.read(RB_N1V5_CURRENT_INA200_CHANNEL) {
+          Err(err) => {
+            error!("cannot read INA200 (N1V5 CURRENT), {err}");
+          },
+          Ok(_result) => {
+            n1v5_current = _result/ 20.0 / 0.039;
+          }
+        }
         let n1v5_power = n1v5_voltage.abs() * n1v5_current;
 
-        i2c_mux_1.reset().expect("cannot reset PCA9548A");
-        i2c_mux_2.reset().expect("cannot reset PCA9548A");
+        match i2c_mux_1.reset() {
+          Err(err) => {
+            error!("cannot reset PCA9548A, {err}");
+          },
+          Ok(_) => ()
+        }
+        match i2c_mux_2.reset() {
+          Err(err) => {
+            error!("cannot reset PCA9548A, {err}");
+          },
+          Ok(_) => ()
+        }
 
         Self {
             drs_dvdd_voltage,
