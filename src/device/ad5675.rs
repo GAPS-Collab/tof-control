a1e3d38d5f153b686a79c6d2f6aeb85908f0ab33
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2025-12-03 19:35:28 +0000

    UPDATE: debugging in src/device/ad5675.rs

--- src/device/ad5675.rs
+++ src/device/ad5675.rs
@@ -4,6 +4,13 @@ use crate::constant::*;
 use i2cdev::core::*;
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CMessage, LinuxI2CError};
 
+use libc::{ioctl, O_RDWR, STDOUT_FILENO};
+use ratatui::buffer;
+use std::fs::File;
+use std::os::fd::{AsRawFd, IntoRawFd, RawFd};
+use std::os::raw::{c_uint, c_ulong};
+use i2c_linux_sys::*;
+
 const WRITE_AND_UPDATE_DAC: u8 = 0x30;
 const READBACK_ENABLE: u8 = 0x90;
 const POWER_DOWN_UP: u8 = 0x40;
@@ -17,7 +24,29 @@ impl AD5675 {
         Self { bus, address }
     }
     
-    pub fn write_dac(&self, channel:u8, value: u16) -> Result<(), LinuxI2CError> {
+    pub fn write_dac(&self, channel: u8, value: u16) -> Result<(), LinuxI2CError> {
+        let fd = if let Ok(file) = File::open("/dev/i2c-0") {
+            file.into_raw_fd()
+        } else {
+            STDOUT_FILENO
+        };
+        unsafe { ioctl(fd, (I2C_SLAVE as c_ulong).try_into().unwrap(), 0x77) };
+        i2c_linux_sys::i2c_smbus_write_byte(fd, 0x04);
+        unsafe {
+            ioctl(
+                fd,
+                (I2C_SLAVE as c_ulong).try_into().unwrap(),
+                self.address as c_uint,
+            )
+        };
+        let mut buffer = (value & 0xFF00) >> 8;
+        buffer = buffer | (value & 0x00FF) << 8;
+        i2c_linux_sys::i2c_smbus_write_word_data(fd, 0x30 + channel, buffer);
+
+        Ok(())
+    }
+
+    pub fn write_dac_i2cdev(&self, channel:u8, value: u16) -> Result<(), LinuxI2CError> {
         let mut dev = LinuxI2CDevice::new(&format!("/dev/i2c-{}", self.bus), self.address)?;
         
         let dac_value_msb = (value & 0xFF00) >> 8;
